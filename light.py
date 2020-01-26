cbeeecf37c6293af47b5601abbf8e30a7616f938
zomfg <sergio.kunats@gmail.com>
2020-01-26 23:19:52 +0100

    light: type hints, disabled debug logs

--- light.py
+++ light.py
@@ -2,6 +2,7 @@
 import logging
 import re
 import socket
+from typing import Optional
 import voluptuous as vol
 from homeassistant.components.light import (
     ATTR_BRIGHTNESS,
@@ -56,7 +57,7 @@ class PrismatikLight(Light):
         self._apikey = apikey
         self._sock = None
 
-    def _connect(self):
+    def _connect(self) -> bool:
         """Connect to Prismatik server."""
         try:
             self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -71,31 +72,31 @@ class PrismatikLight(Light):
             self._disconnect()
         return self._sock is not None
 
-    def _disconnect(self):
+    def _disconnect(self) -> None:
         """Disconnect from Prismatik server."""
         if self._sock:
             self._sock.shutdown(socket.SHUT_RDWR)
             self._sock.close()
         self._sock = None
 
-    async def async_will_remove_from_hass(self):
+    async def async_will_remove_from_hass(self) -> None:
         """Disconnect from update signal."""
         self._disconnect()
 
-    def _send(self, buffer):
+    def _send(self, buffer: str) -> Optional[str]:
         """Send command to Prismatik server."""
         if self._sock is None and self._connect() is False:
             return None
 
-        _LOGGER.error("SENDING %s", buffer)
+        # _LOGGER.error("SENDING %s", buffer)
         try:
             self._sock.sendall(buffer.encode("ascii"))
             answer = self._sock.recv(4096).decode("ascii").strip()
         except OSError:
-            _LOGGER.error("FAILED %s", buffer)
+            # _LOGGER.error("FAILED %s", buffer)
             self._disconnect()
             return None
-        _LOGGER.error("RECEIVED %s", answer)
+        # _LOGGER.error("RECEIVED %s", answer)
         if answer == "not locked":
             if self._do_cmd("lock"):
                 return self._send(buffer)
@@ -108,7 +109,7 @@ class PrismatikLight(Light):
             answer = None
         return answer
 
-    def _get_cmd(self, cmd):
+    def _get_cmd(self, cmd: str) -> Optional[str]:
         """Execute get-command Prismatik server."""
         answer = self._send("get" + cmd + "\n")
         if answer is not None:
@@ -117,17 +118,16 @@ class PrismatikLight(Light):
                 return matches.group(1)
         return None
 
-    def _set_cmd(self, cmd, value):
+    def _set_cmd(self, cmd: str, value: any) -> bool:
         """Execute set-command Prismatik server."""
-        answer = self._send("set" + cmd + ":" + str(value) + "\n")
-        return answer == "ok"
+        return self._send("set" + cmd + ":" + str(value) + "\n") == "ok"
 
-    def _do_cmd(self, cmd, value=None):
+    def _do_cmd(self, cmd: str, value: any = None) -> bool:
         """Execute other command Prismatik server."""
         answer = self._send(cmd + (":" + str(value) if value else "") + "\n")
         return re.compile(r"^(ok|" + cmd + r":success)$").match(answer) is not None
 
-    def _set_rgb_color(self, rgb):
+    def _set_rgb_color(self, rgb: tuple) -> None:
         """Generate and execude setcolor command on Prismatik server."""
         leds = self.leds
         rgb_color = ','.join(map(str, rgb))
@@ -135,7 +135,7 @@ class PrismatikLight(Light):
         self._set_cmd("color", pixels)
 
     @property
-    def hs_color(self):
+    def hs_color(self) -> Optional[list]:
         """Return first pixel color on Prismatik."""
         pixels = self._get_cmd("colors")
         if pixels is None:
@@ -146,55 +146,54 @@ class PrismatikLight(Light):
         return color_util.color_RGB_to_hs(int(rgb.group(1)), int(rgb.group(2)), int(rgb.group(3)))
 
     @property
-    def name(self):
+    def name(self) -> str:
         """Return the name of the light."""
         return "Prismatik"
 
     @property
-    def available(self):
+    def available(self) -> bool:
         """Return availability of the light."""
         return self._sock is not None
 
     @property
-    def is_on(self):
+    def is_on(self) -> bool:
         """Is this thing on."""
-        status = self._get_cmd("status")
-        return status == "on" if status else None
+        return self._get_cmd("status") == "on"
 
     @property
-    def leds(self):
+    def leds(self) -> int:
         """Return leds of the light."""
         countleds = self._get_cmd("countleds")
         return int(countleds) if countleds else 0
 
     @property
-    def brightness(self):
+    def brightness(self) -> Optional[int]:
         """Return brightness of the light."""
         brightness = self._get_cmd("brightness")
         return int(brightness) * 2.55 if brightness else None
 
     @property
-    def supported_features(self):
+    def supported_features(self) -> int:
         """Flag supported features."""
         return SUPPORT_BRIGHTNESS | SUPPORT_COLOR | SUPPORT_EFFECT
 
     @property
-    def effect_list(self):
+    def effect_list(self) -> Optional[list]:
         """Profiles."""
         profiles = self._get_cmd("profiles")
         return list(filter(None, profiles.split(';'))) if profiles else None
 
     @property
-    def effect(self):
+    def effect(self) -> Optional[str]:
         """Current profile."""
         return self._get_cmd("profile")
 
-    def turn_on(self, **kwargs):
+    def turn_on(self, **kwargs) -> None:
         """Turn the light on."""
         self._set_cmd("mode", "moodlight")
         self._set_cmd("persistonunlock", "on")
         self._set_cmd("status", "on")
-        _LOGGER.error("TURNING ON WITH %s", *kwargs)
+        # _LOGGER.error("TURNING ON WITH %s", *kwargs)
         if ATTR_HS_COLOR in kwargs:
             rgb = color_util.color_hs_to_RGB(*kwargs[ATTR_HS_COLOR])
             self._set_rgb_color(rgb)
@@ -203,9 +202,9 @@ class PrismatikLight(Light):
         elif ATTR_EFFECT in kwargs:
             self._set_cmd("profile", kwargs[ATTR_EFFECT])
 
-    def turn_off(self, **kwargs):
+    def turn_off(self, **kwargs) -> None:
         """Turn the light off."""
         #pylint: disable=unused-argument
-        _LOGGER.error("TURNING OFF WITH %s", *kwargs)
+        # _LOGGER.error("TURNING OFF WITH %s", *kwargs)
         self._set_cmd("status", "off")
         self._do_cmd("unlock")
