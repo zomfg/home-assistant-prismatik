2887adab27d1eaa2030383be2097d633f8e8ffd1
zomfg <sergio.kunats@gmail.com>
2020-01-27 15:51:22 +0100

    light: moved API literals to Enum

--- light.py
+++ light.py
@@ -2,6 +2,7 @@
 import logging
 import re
 import socket
+from enum import Enum
 from typing import Optional
 
 import homeassistant.helpers.config_validation as cv
@@ -45,6 +46,44 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     apikey = config.get(CONF_API_KEY)
 
     add_entities([PrismatikLight(hass, name, address, apikey)])
+
+
+class PrismatikAPI(Enum):
+    """Prismatik API literals."""
+
+    CMD_LOCK = "lock"
+    CMD_UNLOCK = "unlock"
+    CMD_GETCOLOR = "colors"
+    CMD_SETCOLOR = "color"
+    CMD_APIKEY = "apikey"
+    CMD_GETPROFILE = "profile"
+    CMD_SETPROFILE = CMD_GETPROFILE
+    CMD_GETPROFILES = "profiles"
+    CMD_GETBRIGHTNESS = "brightness"
+    CMD_SETBRIGHTNESS = CMD_GETBRIGHTNESS
+    CMD_GETSTATUS = "status"
+    CMD_SETSTATUS = CMD_GETSTATUS
+    CMD_GETCOUNTLEDS = "countleds"
+    CMD_PERSISTONUNLOCK = "persistonunlock"
+    CMD_GETMODE = "mode"
+    CMD_SETMODE = CMD_GETMODE
+
+    AWR_OK = "ok"
+    AWR_SUCCESS = "success"
+    AWR_NOTLOCKED = "not locked"
+    AWR_AUTHREQ = "authorization required"
+
+    STS_ON = "on"
+    STS_OFF = "off"
+
+    MOD_MOODLIGHT = "moodlight"
+
+    def __str__(self) -> str:
+        return self.value
+
+    def __eq__(self, other: str) -> bool:
+        # pylint: disable=comparison-with-callable
+        return self.value == other
 
 
 class PrismatikLight(Light):
@@ -98,19 +137,19 @@ class PrismatikLight(Light):
             self._disconnect()
             return None
         # _LOGGER.error("RECEIVED %s", answer)
-        if answer == "not locked":
-            if self._do_cmd("lock"):
+        if answer == PrismatikAPI.AWR_NOTLOCKED:
+            if self._do_cmd(PrismatikAPI.CMD_LOCK):
                 return self._send(buffer)
             _LOGGER.error("Could not lock Prismatik")
             answer = None
-        if answer == "authorization required":
-            if self._apikey and self._do_cmd("apikey", self._apikey):
+        if answer == PrismatikAPI.AWR_AUTHREQ:
+            if self._apikey and self._do_cmd(PrismatikAPI.CMD_APIKEY, self._apikey):
                 return self._send(buffer)
             _LOGGER.error("Could not lock Prismatik")
             answer = None
         return answer
 
-    def _get_cmd(self, cmd: str) -> Optional[str]:
+    def _get_cmd(self, cmd: PrismatikAPI) -> Optional[str]:
         """Execute get-command Prismatik server."""
         answer = self._send(f"get{cmd}\n")
         if answer is not None:
@@ -119,27 +158,32 @@ class PrismatikLight(Light):
                 return matches.group(1)
         return None
 
-    def _set_cmd(self, cmd: str, value: any) -> bool:
+    def _set_cmd(self, cmd: PrismatikAPI, value: any) -> bool:
         """Execute set-command Prismatik server."""
-        return self._send(f"set{cmd}:{value}\n") == "ok"
+        return self._send(f"set{cmd}:{value}\n") == PrismatikAPI.AWR_OK
 
-    def _do_cmd(self, cmd: str, value: Optional[any] = None) -> bool:
+    def _do_cmd(self, cmd: PrismatikAPI, value: Optional[any] = None) -> bool:
         """Execute other command Prismatik server."""
         value = f":{value}" if value else ""
         answer = self._send(f"{cmd}{value}\n")
-        return re.compile(fr"^(ok|{cmd}:success)$").match(answer) is not None
+        return (
+            re.compile(
+                fr"^(PrismatikAPI.AWR_OK|{cmd}:{PrismatikAPI.AWR_SUCCESS})$"
+            ).match(answer)
+            is not None
+        )
 
     def _set_rgb_color(self, rgb: tuple) -> None:
         """Generate and execude setcolor command on Prismatik server."""
         leds = self.leds
         rgb_color = ",".join(map(str, rgb))
         pixels = ";".join(list([f"{i}-{rgb_color}" for i in range(1, leds + 1)]))
-        self._set_cmd("color", pixels)
+        self._set_cmd(PrismatikAPI.CMD_SETCOLOR, pixels)
 
     @property
     def hs_color(self) -> Optional[list]:
         """Return first pixel color on Prismatik."""
-        pixels = self._get_cmd("colors")
+        pixels = self._get_cmd(PrismatikAPI.CMD_GETCOLOR)
         if pixels is None:
             return None
         rgb = re.match(r"^\d+-(\d+),(\d+),(\d+);", pixels)
@@ -162,18 +206,18 @@ class PrismatikLight(Light):
     @property
     def is_on(self) -> bool:
         """Is this thing on."""
-        return self._get_cmd("status") == "on"
+        return self._get_cmd(PrismatikAPI.CMD_GETSTATUS) == PrismatikAPI.STS_ON
 
     @property
     def leds(self) -> int:
         """Return leds of the light."""
-        countleds = self._get_cmd("countleds")
+        countleds = self._get_cmd(PrismatikAPI.CMD_GETCOUNTLEDS)
         return int(countleds) if countleds else 0
 
     @property
     def brightness(self) -> Optional[int]:
         """Return brightness of the light."""
-        brightness = self._get_cmd("brightness")
+        brightness = self._get_cmd(PrismatikAPI.CMD_GETBRIGHTNESS)
         return int(brightness) * 2.55 if brightness else None
 
     @property
@@ -184,31 +228,33 @@ class PrismatikLight(Light):
     @property
     def effect_list(self) -> Optional[list]:
         """Profiles."""
-        profiles = self._get_cmd("profiles")
+        profiles = self._get_cmd(PrismatikAPI.CMD_GETPROFILES)
         return list(filter(None, profiles.split(";"))) if profiles else None
 
     @property
     def effect(self) -> Optional[str]:
         """Current profile."""
-        return self._get_cmd("profile")
+        return self._get_cmd(PrismatikAPI.CMD_GETPROFILE)
 
     def turn_on(self, **kwargs) -> None:
         """Turn the light on."""
-        self._set_cmd("mode", "moodlight")
-        self._set_cmd("persistonunlock", "on")
-        self._set_cmd("status", "on")
+        self._set_cmd(PrismatikAPI.CMD_SETMODE, PrismatikAPI.MOD_MOODLIGHT)
+        self._set_cmd(PrismatikAPI.CMD_PERSISTONUNLOCK, PrismatikAPI.STS_ON)
+        self._set_cmd(PrismatikAPI.CMD_SETSTATUS, PrismatikAPI.STS_ON)
         # _LOGGER.error("TURNING ON WITH %s", *kwargs)
         if ATTR_HS_COLOR in kwargs:
             rgb = color_util.color_hs_to_RGB(*kwargs[ATTR_HS_COLOR])
             self._set_rgb_color(rgb)
         elif ATTR_BRIGHTNESS in kwargs:
-            self._set_cmd("brightness", int(kwargs[ATTR_BRIGHTNESS] / 2.55))
+            self._set_cmd(
+                PrismatikAPI.CMD_SETBRIGHTNESS, int(kwargs[ATTR_BRIGHTNESS] / 2.55)
+            )
         elif ATTR_EFFECT in kwargs:
-            self._set_cmd("profile", kwargs[ATTR_EFFECT])
+            self._set_cmd(PrismatikAPI.CMD_SETPROFILE, kwargs[ATTR_EFFECT])
 
     def turn_off(self, **kwargs) -> None:
         """Turn the light off."""
         # pylint: disable=unused-argument
         # _LOGGER.error("TURNING OFF WITH %s", *kwargs)
-        self._set_cmd("status", "off")
-        self._do_cmd("unlock")
+        self._set_cmd(PrismatikAPI.CMD_SETSTATUS, PrismatikAPI.STS_OFF)
+        self._do_cmd(PrismatikAPI.CMD_UNLOCK)
