f81049d27f096f40fde94c34d0c9476deb997bbc
zomfg <239811+zomfg@users.noreply.github.com>
2021-11-07 22:53:34 +0100

    test server: cleanup

--- test_server.py
+++ test_server.py
@@ -1,44 +1,55 @@
 #!/usr/bin/env python
+"""Smol test server mimicking Prismatik"""
 import socket
-import sys
 
-localIP     = "127.0.0.1"
-localPort   = 3636
+LOCAL_IP   = "127.0.0.1"
+LOCAL_PORT = 3636
 
 def welcome():
+    """prismatik api welcome message"""
     return "Lightpack API v1.4 - Prismatik API v2.2 (type 'help' for more info)\n"
 
-# getcolors
-# colors:0-5,255,1;1-1,255,9;2-1,255,22;3-1,255,35;...
-leds = 10
-color = '255,255,255'
+LEDS = 10
+COLOR = '255,255,255'
 def getcolors():
-    colors = ";".join([f"{idx}-{color}" for idx in range(leds)])
+    """
+    getcolors
+    colors:0-5,255,1;1-1,255,9;2-1,255,22;3-1,255,35;...
+    """
+    colors = ";".join([f"{idx}-{COLOR}" for idx in range(LEDS)])
     return f"colors:{colors};\n"
 
-# getstatus
-# status:on
-status = "on"
+STATUS = "on"
 def getstatus():
-    return f"status:{status}\n"
+    """
+    getstatus
+    status:on
+    """
+    return f"status:{STATUS}\n"
 
-# getbrightness
-# brightness:100
-brightness = 100
+BRIGHTNESS = 100
 def getbrightness():
-    return f"brightness:{brightness}\n"
+    """
+    getbrightness
+    brightness:100
+    """
+    return f"brightness:{BRIGHTNESS}\n"
 
-profiles = ['Lightpack','Призматик','Regnbåge']
-profile_idx = 0
-# getprofile
-# profile:hassio
+PROFILES = ['Lightpack','Призматик','Regnbåge']
+PROFILE_IDX = 0
 def getprofile():
-    return f"profile:{profiles[profile_idx]}\n"
+    """
+    getprofile
+    profile:hassio
+    """
+    return f"profile:{PROFILES[PROFILE_IDX]}\n"
 
-# getprofiles
-# profiles:hassio;Lightpack;
 def getprofiles():
-    return f"profiles:{';'.join(profiles)};\n"
+    """
+    getprofiles
+    profiles:hassio;Lightpack;
+    """
+    return f"profiles:{';'.join(PROFILES)};\n"
 
 # print all test responses
 print(welcome(), end='')
@@ -49,7 +60,7 @@ print(getbrightness(), end='')
 print(getcolors(), end='')
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((localIP, localPort))
+s.bind((LOCAL_IP, LOCAL_PORT))
 s.listen(4)
 client, addr = s.accept()
 client.sendall(welcome().encode())
@@ -60,7 +71,7 @@ try:
         resp = globals()[req]()
         # print(resp.strip())
         client.sendall(resp.encode())
-except:
+except: # pylint: disable=bare-except
     pass
 finally:
     s.close()
